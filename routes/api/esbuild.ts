5792ba0daa64f3305bf49a094e1959d6e4a5515b
Christophe Massolin <christophe.massolin@gmail.com>
2024-01-21 01:46:17 +0000

    disable esbuild on dpeloy

--- routes/api/esbuild.ts
+++ routes/api/esbuild.ts
@@ -2,9 +2,11 @@ import { Hono } from "hono/mod.ts";
 
 const app = new Hono();
 
-// Poll deployment data from Subhosting API
-app.get("/", async (ctx) => {
-  return ctx.redirect("http://localhost:3000/esbuild");
-});
+if (Deno.args.includes("--dev")) {
+  // Poll deployment data from Subhosting API
+  app.get("/", async (ctx) => {
+    return ctx.redirect("http://localhost:3000/esbuild");
+  });
+}
 
 export default app;
